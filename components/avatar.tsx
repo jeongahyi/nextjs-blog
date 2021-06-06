752504d1bd972f4321e7d6ab2b3996e9d8cf90e1
Jeongah Yi <jeongahyi@gmail.com>
2021-06-06 19:25:28 +0900

    Add types to Avatar Component

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,4 +1,4 @@
-export default function Avatar({ name, picture }) {
+export default function Avatar({ name, picture }: { name: string, picture: string }) {
     return (
         <div className="flex items-center">
             <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name}/>
