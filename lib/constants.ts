5f2e2a26b8ef6be9a4c0eb3be1e68c38096b96c2
Jeongah Yi <jeongahyi@gmail.com>
2021-07-15 11:05:00 +0900

    Change title typo

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const NAME = 'Jeongah Y.'
-export const SITE_TITLE = 'Blog.'
+export const SITE_TITLE = 'Blog.jay'
 export const INTRO = 'I want to leave my footprints on the sands of time know there was something that I left behind when I leave this world. - I was here by Beyonce'
 export const GITHUB_PATH = 'https://github.com/jeongahyi'
